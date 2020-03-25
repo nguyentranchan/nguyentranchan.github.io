c687c76e1e5a3d0464f19b4ce29558b7cff3fd30
LeTrinh <touken2411@gmail.com>
2020-03-25 13:48:25 +0700

    add demo data

---
+++ js/recruitData.js
@@ -0,0 +1,91 @@
+var data = {
+    qa: {
+        color:'#E64C3C',
+        name:'QA/QC',
+        experience:'0 - 2 years',
+        tags: [
+            'Excel', 'SQL'
+        ],
+        requirement: [
+            '- MUST be fluent in English – both spoken and written',
+            '- Understand domain knowledge & client testing process clearly to implement all testing activities independently and effectively',
+            '- Able to understand, communicate, analyse requirements, and identify gaps in requirements',
+            '- Good at analyzing information objectively and making reasonable decisions',
+            '- Able to perform all kinds of testing effectively & independently',
+            '- Able to manage time and prioritize multiple tasks',
+            '- Able to perform testing under high pressure'
+        ],
+        more: [
+            '- Experience in creating and implementing test cases/scripts based on requirements',
+            '- Experience in estimating testing activities',
+            '- Good knowledge and experience in automation tests'
+        ]
+    },
+    fe: {
+        color:'#EDC951',
+        name:'Front-end Developer',
+        experience:'0 - 1 years',
+        tags: [
+            'Excel', 'SQL'
+        ],
+        requirement: [
+            '- MUST be fluent in English – both spoken and written',
+            '- Understand domain knowledge & client testing process clearly to implement all testing activities independently and effectively',
+            '- Able to understand, communicate, analyse requirements, and identify gaps in requirements',
+            '- Good at analyzing information objectively and making reasonable decisions',
+            '- Able to perform all kinds of testing effectively & independently',
+            '- Able to manage time and prioritize multiple tasks',
+            '- Able to perform testing under high pressure'
+        ],
+        more: [
+            '- Experience in creating and implementing test cases/scripts based on requirements',
+            '- Experience in estimating testing activities',
+            '- Good knowledge and experience in automation tests'
+        ]
+    },
+    be: {
+        color:'#2A4D69',
+        name:'Back-end Developer',
+        experience:'1 - 2 years',
+        tags: [
+            'Excel', 'SQL'
+        ],
+        requirement: [
+            '- MUST be fluent in English – both spoken and written',
+            '- Understand domain knowledge & client testing process clearly to implement all testing activities independently and effectively',
+            '- Able to understand, communicate, analyse requirements, and identify gaps in requirements',
+            '- Good at analyzing information objectively and making reasonable decisions',
+            '- Able to perform all kinds of testing effectively & independently',
+            '- Able to manage time and prioritize multiple tasks',
+            '- Able to perform testing under high pressure'
+        ],
+        more: [
+            '- Experience in creating and implementing test cases/scripts based on requirements',
+            '- Experience in estimating testing activities',
+            '- Good knowledge and experience in automation tests'
+        ]
+    },
+    de: {
+        color:'#F2F5D0',
+        name:'Web Designer',
+        experience:'1 - 2 years',
+        tags: [
+            'Excel', 'SQL'
+        ],
+        requirement: [
+            '- MUST be fluent in English – both spoken and written',
+            '- Understand domain knowledge & client testing process clearly to implement all testing activities independently and effectively',
+            '- Able to understand, communicate, analyse requirements, and identify gaps in requirements',
+            '- Good at analyzing information objectively and making reasonable decisions',
+            '- Able to perform all kinds of testing effectively & independently',
+            '- Able to manage time and prioritize multiple tasks',
+            '- Able to perform testing under high pressure'
+        ],
+        more: [
+            '- Experience in creating and implementing test cases/scripts based on requirements',
+            '- Experience in estimating testing activities',
+            '- Good knowledge and experience in automation tests'
+        ]
+    },
+    
+}
