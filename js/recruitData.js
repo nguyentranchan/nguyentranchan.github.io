fac89bc645c5d7292a3eef375950d8c0f7477bb0
touken2411 <43944044+touken2411@users.noreply.github.com>
2020-03-25 17:08:44 +0700

    change job list

--- js/recruitData.js
+++ js/recruitData.js
@@ -2,6 +2,7 @@ var data = {
     qa: {
         color:'#E64C3C',
         name:'QA/QC',
+        position:'Fresher',
         experience:'0 - 2 years',
         tags: [
             'Excel', 'SQL'
@@ -24,9 +25,10 @@ var data = {
     fe: {
         color:'#EDC951',
         name:'Front-end Developer',
+        position:'Fresher',
         experience:'0 - 1 years',
         tags: [
-            'Excel', 'SQL'
+            'HTML5', 'CSS3', 'UI/UX', 'VueJS', 'ReactJS',  'JS' 
         ],
         requirement: [
             '- MUST be fluent in English – both spoken and written',
@@ -46,31 +48,33 @@ var data = {
     be: {
         color:'#2A4D69',
         name:'Back-end Developer',
-        experience:'1 - 2 years',
+        position:'Fresher',
+        experience:'0 - 2 years',
         tags: [
-            'Excel', 'SQL'
+            'PHP', 'JS/TS', 'Golang'
         ],
         requirement: [
-            '- MUST be fluent in English – both spoken and written',
-            '- Understand domain knowledge & client testing process clearly to implement all testing activities independently and effectively',
-            '- Able to understand, communicate, analyse requirements, and identify gaps in requirements',
-            '- Good at analyzing information objectively and making reasonable decisions',
-            '- Able to perform all kinds of testing effectively & independently',
-            '- Able to manage time and prioritize multiple tasks',
-            '- Able to perform testing under high pressure'
+            '- In-depth knowledge about data structures and algorithms',
+            '- Deep understand software development principles SOLID, DRY, KISS',
+            '- Experience about web-based technologies',
+            '- Experience about writing SQL efficient',
+            '- Guarantee the quality for your work',
+            '- Fluent English – both spoken and written'
         ],
         more: [
-            '- Experience in creating and implementing test cases/scripts based on requirements',
-            '- Experience in estimating testing activities',
-            '- Good knowledge and experience in automation tests'
+            '- Good at analytical and organization skills',
+            '- Experience in developing web application vs Node.js',
+            '- Self-adaptive with new technologies quickly',
+            '- Can work independant without/less guidance'
         ]
     },
     de: {
         color:'#F2F5D0',
         name:'Web Designer',
+        //position:'Fresher',
         experience:'1 - 2 years',
         tags: [
-            'Excel', 'SQL'
+            'HTML5', 'CSS3', 'JS', 'VueJS', 'ReactJS', 'UI/UX'
         ],
         requirement: [
             '- MUST be fluent in English – both spoken and written',
