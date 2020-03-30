38a5e1304dcfb69a778570e0ee5d4ae5a2a05904
15110091 <lychautrinha@gmail.com>
2020-03-30 16:22:48 +0700

    update sendCV

--- app.js
+++ app.js
@@ -21,22 +21,50 @@ const smtpTransport = nodemailer.createTransport({
 app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(cors())
+app.use(cors({credentials: true, origin: true}))
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 app.post('/sendMail', (req, res) => {
-    const {email = '', content = ''} = req.body
-    if (!email.trim() && !content.trim()) {
+    const {email = ' ', content = ' '} = req.body
+    if (!email.trim() && !content.trim() && !position.trim()) {
         res.status(400).end('Email or Content is empty')
     }
+    console.log(req.body)
     const mailOptions = {
         from: email,
-        to : 'touken2411@gmail.com',
+        to : 'lychautrinha@gmail.com',
         subject : 'Sending From Landing Page',
         text : content.substr(0, 1000)
+    }
+    smtpTransport.sendMail(mailOptions, function(error, response){
+        if (error){
+            console.log(error);
+            res.status(500).end("error");
+        } else {
+            console.log("Message sent: " + response);
+            res.status(200).end("sent");
+        }
+    });
+})
+
+app.post('/sendCV', (req, res) => {
+    const {email = ' ', message = ' ', name=' ', position=' '} = req.body
+
+    const replytext = `<div>
+    <p><b>Tên:</b> ${name}</p><p>
+    <p><b>Vị trí ứng tuyển:</b> ${position}</p><p>
+    <p><b>Email:</b> ${email}</p><p>
+    <p><b>Tin nhắn:</b> ${message}</p><p>
+    </div>`;
+
+    const mailOptions = {
+        from: email,
+        to : 'lychautrinha@gmail.com',
+        subject : 'Sending From Landing Page',
+        html: replytext,
     }
     smtpTransport.sendMail(mailOptions, function(error, response){
         if (error){
--- js/email.js
+++ js/email.js
@@ -46,3 +46,64 @@ function sendMail() {
         result.innerHTML = 'Oops! Something went wrong.'
     });
 }
+
+function sendCV() {
+    console.log('send')
+    const textName = document.querySelector('.cv-form #name')
+    const textPosition = document.querySelector('.cv-form #position')
+    const textarea = document.querySelector('.cv-form textarea')
+    const emailInput = document.querySelector('.cv-form #email')
+    const email = (emailInput.value || '').trim()
+    const message = (textarea.value || '').trim()
+    const name = (textName.value || '').trim()
+    const position = (textPosition.value || '').trim()
+    const emailError = document.querySelector('.cv-form #email-error')
+    const contentError = document.querySelector('.cv-form #content-error')
+    const nameError = document.querySelector('.cv-form #name-error')
+    const result = document.querySelector('.cv-form #result')
+    // console.log([textarea,textName,emailInput])
+    if (!email) {
+        emailError.innerHTML = 'Email is required.'
+        emailInput.classList.add('required')
+        return
+    } else if (!validateEmail(email)) {
+        emailError.innerHTML = 'Please enter a valid email.'
+        emailInput.classList.add('required')
+        return
+    }
+
+    if (!name) {
+        nameError.innerHTML = 'Name content is required.'
+        textarea.classList.add('required')
+        return
+    }
+
+    if (!message) {
+        contentError.innerHTML = 'Message content is required.'
+        textarea.classList.add('required')
+        return
+    }
+
+    emailError.innerHTML = ''
+    nameError.innerHTML = ''
+    contentError.innerHTML = ''
+    textarea.classList.remove('required')
+    emailInput.classList.remove('required')
+    textName.classList.remove('required')
+
+    $.ajax({
+        url: "/sendCV",
+        data: JSON.stringify({email, message,name,position}),
+        type: 'POST',
+        contentType: 'application/json'
+    })
+    .done(function(data) {
+        result.classList.add('result-success')
+        result.innerHTML = 'Thank you for contacting us, we will touch base you within 24 hours.'
+    }).fail(function (error) {
+        result.classList.add('result-error')
+        result.innerHTML = 'Oops! Something went wrong.'
+    });
+}
+
+
