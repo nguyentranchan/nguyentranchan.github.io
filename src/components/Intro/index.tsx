e0a4a5ae565c7ac068cab2504f10b63f3fedad53
Khiet Tran <vankhiet.tran@gmail.com>
2020-04-16 15:43:19 +0700

    Condition displaying slogan

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -47,12 +47,14 @@ export class Intro extends React.Component<IntroProps, { width: number; rendered
       <div className="intro relative bg-gblue flex justify-center">
         {displayParticle && <Particles className="absolute top-0 right-0 left-0 bottom-0 z-10" params={params} />}
         <div className="relative gcontainer">
-          <div className="absolute text-white slogan md:slogan-md slogan-xs gcontainer">
-            <p className={`block md:text-3xl lg:text-4xl text-2xl text-center md:text-left ${titleClass}`}>{title}</p>
-            {description && <p className="text-center md:text-left">{description}</p>}
-          </div>
           {this.state.rendered && (
             <React.Fragment>
+              <div className="absolute text-white slogan md:slogan-md slogan-xs gcontainer">
+                <p className={`block md:text-3xl lg:text-4xl text-2xl text-center md:text-left ${titleClass}`}>
+                  {title}
+                </p>
+                {description && <p className="text-center md:text-left">{description}</p>}
+              </div>
               {width >= 768 && <IntroImage className="w-full h-full gcontainer" />}
               {width < 768 && <IntroImageMobile className="w-full h-full block" />}
             </React.Fragment>
