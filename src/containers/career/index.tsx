d72ab363f2357dd1024c5eff2f888cf2d4ee8c0c
Kai Tran <vankhiet.tran@gmail.com>
2020-04-21 13:52:25 +0700

    LưÆợm lÄặt content, change the layout of career section

--- src/containers/career/index.tsx
+++ src/containers/career/index.tsx
@@ -1,13 +1,6 @@
 import React from 'react'
 import JoinUs from '../../images/joinus.svg'
-import ba from '../../images/career/ba.png'
-import be from '../../images/career/be.png'
-//import fe from '../../images/career/fe.png'
-
 export class Career extends React.Component {
-  gotoCareerPage() {
-    window.location.his
-  }
   render() {
     return (
       <React.Fragment>
@@ -17,46 +10,32 @@ export class Career extends React.Component {
             <h1 className="text-4xl text-center text-blue-800">Early Career</h1>
             <div className="md:inline-flex pt-4">
               <div className="md:w-1/2 flex flex-col md:mr-4">
-                <h2 className="text-2xl text-blue-800">We need guys willing to learn, work and play</h2>
-                {/*<p className="pt-2 pb-4">*/}
-                {/*  Aenean odio ex, aliquet id scelerisque finibus, luctus eu mi. Sed in erat sed dui faucibus suscipit ac*/}
-                {/*  sit amet nisl. Ut non lacinia urna. Quisque id nulla quis mauris ultricies viverra.*/}
-                {/*</p>*/}
+                <h2 className="text-2xl text-blue-800 pb-2">We need guys willing to learn, work and play</h2>
                 <JoinUs className="w-full h-full" />
               </div>
-              <div className="md:w-1/2 pt-4">
+              <div className="md:w-1/2 pt-4 md:pt-0 border-0 md:border-l-2 border-none md:border-solid">
                 <div className="job inline-flex">
-                  <img src={ba} alt="" style={{ width: 8, height: 120 }} />
                   <div className="flex flex-col pl-4">
-                    <h3 className="text-xl">Backend Developer</h3>
+                    <h2 className="text-xl">Backend Developer</h2>
                     <p>
-                      Vivamus a turpis non ligula scelerisque bibendum non et lectus. Quisque ullamcorper, purus sit
-                      amet tempor tristique, mauris metus facilisis elit, at pulvinar magna orci in ex.{' '}
+                      We are looking for an experienced Back-end developer to join our IT team! As a Back-end Developer,
+                      you will be responsible for the server-side web application logic as well as for the integration
+                      of the front-end part.
                     </p>
                   </div>
                 </div>
                 <div className="job inline-flex mt-4 mb-6">
-                  <img src={be} alt="" style={{ width: 8, height: 120 }} />
                   <div className="flex flex-col pl-4">
-                    <h3 className="text-xl">Fontend Developer</h3>
+                    <h2 className="text-xl">Fontend Developer</h2>
                     <p>
-                      Vivamus a turpis non ligula scelerisque bibendum non et lectus. Quisque ullamcorper, purus sit
-                      amet tempor tristique, mauris metus facilisis elit, at pulvinar magna orci in ex.{' '}
+                      We are looking for an ambitious and self-driven Front-end Developer to join our team. As a
+                      Front-end Developer, you will be responsible for implementing visual elements that users see and
+                      interact within a web application, which makes your role crucial for the success of our business.
                     </p>
                   </div>
                 </div>
-                {/*<div className="job inline-flex mt-4 mb-6">*/}
-                {/*  <img src={fe} alt="" style={{ width: 8, height: 120 }} />*/}
-                {/*  <div className="flex flex-col pl-4">*/}
-                {/*    <h3 className="text-xl">Business Analyist</h3>*/}
-                {/*    <p>*/}
-                {/*      Vivamus a turpis non ligula scelerisque bibendum non et lectus. Quisque ullamcorper, purus sit*/}
-                {/*      amet tempor tristique, mauris metus facilisis elit, at pulvinar magna orci in ex.{' '}*/}
-                {/*    </p>*/}
-                {/*  </div>*/}
-                {/*</div>*/}
-                <a className="bg-blue-700 hover:bg-blue-600 text-white py-2 px-4" style={{ width: 120 }} href="/career">
-                  See all
+                <a className="text-blue-700 md:pl-4 text-xl hover:underline inline flex items-center" href="/career">
+                  Take your first step to a career with us ➔
                 </a>
               </div>
             </div>
